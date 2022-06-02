c302466c9dc44bea717d97a1c6874f620f91e9ca
Marc Rosanes <marcrosanes@gmail.com>
2022-06-02 10:52:47 +0200

    Compute Magnetic Field Bx', By', Bz'
    B'(r') = R.B(R^(-1)r')
    Compute Magnetic Field in the points of the LoS cube, and
    expressed also in the coordinates of the LoS.
    
    LoS: Line of Sight

---
+++ magneticfield.py
@@ -0,0 +1,167 @@
+import numpy as np
+import pprint
+import matplotlib.pyplot as plt
+
+pp = pprint.PrettyPrinter(indent=4)
+
+##############################################################################
+# Constants used along the script:
+
+# Jupyter Radius ~ Brown Dwarf Radius
+Rj = 7e7 # in meters [m]
+
+# UCD Radius
+R = R_ucd = 1*Rj
+
+Bp = 1e4 # Gauss [G] ; Strength of the B at the pole of the star
+
+# Dipole Magnetic Field Definitions in the magnetic field frame:
+# Bx = 3m xz/r^5
+# By = 3m yz/r^5
+# Bz = m(3z^2/r^5 - 1/r^3)
+
+# With Magnetic Momentum:  m = 1/2 (Bp Rs) 
+m = 1/2 * Bp * R_ucd 
+print(m)
+
+# Total lenght of the cube:
+# L = 20 * R_ucd
+L = 20
+
+# Angles in degrees:
+beta = 30 # Angle from rotation to magnetic axis
+phi = rotation = 0
+inc = inclination = 70 
+# Note: orbits with the rotation axis in the plane of the sky, does not modify 
+# the coordinates system
+
+# Angles to radians:
+b_r = b_rad = np.deg2rad(beta)
+p_r = p_rad = np.deg2rad(phi)
+i_r = i_rad = np.deg2rad(inc)
+
+"""
+Notes about Rotation Matrices:
+
+R = R3 . R2 . R1 to go from vectors expressed in (x, y, z), to vectors
+   expressed in (x', y', z')
+   
+R^(-1) = R1^(-1) . R2^(-1) . R3^(-1) to go from vectors expressed 
+   in (x', y', z'), to vectors expressed in (x, y, z)
+"""
+
+"""
+Notes about magnetic field
+Units of magnetic field in Gauss: 
+Question: Shall I use the distances in meters or in Star Radiuses?
+"""
+
+##############################################################################
+
+x_ = np.linspace(-L/2, L/2, 3)
+y_ = np.linspace(-L/2, L/2, 3)
+z_ = np.linspace(-L/2, L/2, 3)
+
+x, y, z = np.meshgrid(x_, y_, z_)
+vectors = []
+
+# Vectors of the LoS (Line of Sight) cube, expressed in the LoS coordinates
+# Convert meshgrid to array of 3D vectors. Each vector contains the 
+# geometric coordinates of a 3D point in the mesh. The coordinates
+# of each vector represents the center of each of the voxels of the grid.
+for i in range(0,3):
+    for j in range(0,3):
+        for k in range(0,3):
+            vectors.append([x[i,j,k], y[i,j,k], z[i,j,k]])
+           
+for vector in vectors:
+    pp.pprint(vector)
+
+"""
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(111, projection='3d')
+for vector in vectors:
+    ax.scatter(vector[0], vector[1], vector[2], color='b')   
+plt.show()
+"""
+
+###############################################
+
+# Trigonometric functions of the used angles
+sin_b = np.round(np.sin(b_r),4)
+cos_b = np.round(np.cos(b_r),4)
+
+sin_p = np.round(np.sin(p_r),4)
+cos_p = np.round(np.cos(p_r),4)
+
+sin_i = np.round(np.sin(i_r),4)
+cos_i = np.round(np.cos(i_r),4)
+
+# Beta angle from magnetic axis to rotation axis (b = beta)
+R1 = np.array([[ cos_b,  0,  sin_b ], 
+               [ 0,      1,  0     ], 
+               [-sin_b, 0,  cos_b  ]])
+
+# Rotation of the star angle (p = phi = rot)
+R2 = np.array([[ cos_p, -sin_p, 0  ], 
+               [ sin_p, cos_p,  0  ], 
+               [ 0,     0,      1  ]])
+
+# Inclination (i = inc) of the orbit, from line of sight to rotation axis
+R3 = np.array([[ sin_i,  0,  cos_i ], 
+               [ 0,      1,  0     ], 
+               [-cos_i,  0,  sin_i ]])
+
+# Rotation Matrices for each degree of freedom
+R1_inv = R1.transpose()
+R2_inv = R2.transpose()
+R3_inv = R3.transpose()
+
+# Rotation Matrices (Complete Rotation)
+R = R3.dot(R2).dot(R1)
+R_inv = R1_inv.dot(R2_inv).dot(R3_inv)
+
+# vectors_LoS_in_B: Vectors of the Line of Sight cube (LoS coordinates), 
+#   expressed in the magnetic coordinates
+vectors_LoS_in_B = []
+for vector in vectors:
+    vec_LoS_in_B = R_inv.dot(vector)
+    vectors_LoS_in_B.append(vec_LoS_in_B)
+# pp.pprint(vectors_LoS_in_B)    
+
+# B = [Bx, By, Bz], in the points given by the meshgrid of the LoS cube
+Bs_LoS = []
+for vec_LoS_in_B in vectors_LoS_in_B:
+    x = vec_LoS_in_B[0] 
+    y = vec_LoS_in_B[1]
+    z = vec_LoS_in_B[2]
+    if (x==0 and y==0 and z==0):
+        continue
+    r = np.sqrt(x**2 + y**2 + z**2)
+    # Compute Bx, By, Bz in the points of the meshgrid given by the LoS cube,
+    #   but expressed in magnetic coordinates x, y, z 
+    Bx = 3*m * (x*z/r**5)
+    By = 3*m * (y*z/r**5)
+    Bz = m * (3*z**2/r**5 - 1/r**3)
+    # Magnetic Field Vector B in Magnetic Coordinates System (x, y, z)
+    B = np.array([Bx, By, Bz])
+    # Magnetic Field Vector B in the LoS Coordinates System
+    B_LoS = R.dot(B)
+    Bs_LoS.append(B_LoS)
+
+print()    
+pp.pprint(Bs_LoS)
+    
+fig = plt.figure(figsize=(8,8))   
+ax = fig.add_subplot(111, projection='3d')
+for vector in vectors_LoS_in_B:
+    ax.scatter(vector[0], vector[1], vector[2], color='b')   
+plt.show()
+
+
+
+
+
+    
+
+
