86e5eeb256f21e6a8970b6977b3f8416155f4973
Marc Rosanes <marcrosanes@gmail.com>
2022-06-16 19:31:41 +0200

    Add file for finding electrondensity
    First commit with a really basic structure in the file

---
+++ electrondensity.py
@@ -0,0 +1,20 @@
+
+"""
+Knowing "Ra" and defining "l", look for the points of the grid that are
+inside the middle magnetosphere, that are the ones which have a contribution
+in the radio (non-thermal) emission. If possible, paint these points in
+another color, in order to know that the computations are correct
+"""
+
+
+class UCD:
+    """UCD object"""
+
+    def compute_electron_density():
+        print("Electron density")
+        return "hi"
+
+
+ucd = UCD()
+ucd_hiho = ucd.compute_electron_density()
+print(ucd_hiho)
