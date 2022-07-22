3abdc1196b363efdf5196c28f6c4b7fbcd9c8530
Marc Rosanes <marcrosanes@gmail.com>
2022-07-22 12:30:51 +0200

    Add computation and plotting of the Ra
    Add computation and plotting of the Alfvén Radius along the
    magnetic equator
    
    This computation takes many hours in a 8Gb RAM computer

--- alfven_radius.py
+++ alfven_radius.py
@@ -21,27 +21,27 @@ The objective(s) of this file are:
   - Compute the Alfvén Radius of a UCD (or other (sub)stellar/stellar) object
 """
 
-import numpy as nup
+import numpy as np
 import pprint
 from sympy import symbols, Eq, solve
-
 
 pp = pprint.PrettyPrinter(indent=4)
 
 # Available data
-Rsun = 696e6  # [m]
-Rs = Rstar = 4*Rsun  # [m]
+Rsun = 6.96e8  # [m]
+Rs = Rucd = Rstar = 4*Rsun  # [m]
 vinf = 600e3  # [m/s]
-Bp = 1  # 1 Tesla = 1e4 Gauss
+# Bp = 1  # 1 Tesla = 1e4 Gauss
+Bp = 1e4  # Gauss
 bet_degrees = 60
-bet = nup.deg2rad(bet_degrees)  # Angle beta (magnetic vs rotation axis))
+bet = np.deg2rad(bet_degrees)  # Angle beta (magnetic vs rotation axis))
 zet_degrees = 0
-zet = nup.deg2rad(zet_degrees)  # Zeta angle (magnetic longitude)
+zet = np.deg2rad(zet_degrees)  # Zeta angle (magnetic longitude)
 Msun = 2e30  # SolarMass in Kg
 Mlos_ = 1e-9  # Mass Loss [Solar Masses / year]
 Mlos = Mlos_ * Msun / (365*24*3600)  # [Kg / s]
 T = 1 * 24 * 3600  # Period [s]
-w = 2*nup.pi/T
+w = 2*np.pi/T
 mu = magnetic_permeability = 1.256637e-6
 
 # Variable
@@ -50,14 +50,27 @@ r = symbols('r')
 # Formulas
 vw = vinf * (1 - Rs / r)
 B = 1/2 * Bp * (Rs / r)**3
-d = r * nup.sqrt(1 - nup.sin(bet)**2 * nup.cos(zet)**2)
-ro = Mlos / (4 * nup.pi * r**2 * vw)
+ro = Mlos / (4 * np.pi * r**2 * vw)
 
-eq1 = Eq(-B**2/(8*nup.pi) + 1/2 * ro * vw**2 + 1/2 * ro * w**2 * d**2, 0)
-pp.pprint(solve(eq1))
+rotation_phase = zet_array = np.deg2rad(np.array(range(0, 361, 5)))
+alfven_radius_array = []
+for zet in zet_array:
+    d = r * np.sqrt(1 - np.sin(bet)**2 * np.cos(zet)**2)
+    eq1 = Eq(-B**2/(8*np.pi) + 1/2 * ro * vw**2 + 1/2 * ro * w**2 * d**2, 0)
+    solutions = solve(eq1)
+    # pp.pprint(solve(eq1))
+    for solution in solutions:
+        if solution.is_real:
+            if solution >= 0 and solution > 2*Rs:
+                alfven_radius_array.append(solution)
+
+print(alfven_radius_array)
 
 
-aa = 4*Mlos*w**2*(1-(nup.sin(bet))**2*(nup.cos(zet))**2)
+
+
+"""
+aa = 4*Mlos*w**2*(1-(np.sin(bet))**2*(np.cos(zet))**2)
 bb = 4*Mlos*(vinf**2)
 cc = -8*Mlos*Rs*(vinf**2)
 dd = 4*Mlos*(Rs**2)*(vinf**2)
@@ -66,27 +79,6 @@ ff = (Bp**2)*(Rs**7)*vinf/mu
 
 eq2 = Eq(aa * r**7 + bb * r**5 + cc * r**4 + dd * r**3 + ee * r + ff, 0)
 pp.pprint(solve(eq2))
-
-"""
-Other formulas and constants
-kB = 1.380649e-23  # Boltzmann constant [J/K]
-np = 1e7  # [cm^(-3)]
-Tp = 1e6  # [K]
-
-L = (Bp**2 / (16 * nup.pi * np * kB * Tp)) ** (1/6)
 """
 
-"""
-Inner magnetosphere:
-r < Ralfvén
 
-Middle magnetosphere:
-r > Ralfvén
-r < Ralfvén + l
-
-Outter magnetosphere
-r > Ralfvén + l
-
-with the equation of a field line:
-r = L [cos(lambda)]^2
-"""
