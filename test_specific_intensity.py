60cafeb44b06750b6bb8dabcde9310cca0a9789c
Marc Rosanes <marcrosanes@gmail.com>
2022-09-01 19:15:12 +0200

    Add files with function calls for testing them
    Add files with main function calls, in order to test
    these functions faster, than using the GUI.
    
    Files for testing purposes.

---
+++ test_specific_intensity.py
@@ -0,0 +1,8 @@
+
+from object.object_main_func import specific_intensities_2D
+
+specific_intensities_2D(
+        L=30, n=60, beta=65, rotation_angle=0, inclination=25,
+        Robj_Rsun_scale=4, Bp=7700, Pr=1, D_pc=373, f=5e9, Ra=14,
+        l_middlemag=4, δ=2, r_ne=0.002, v_inf=600, plot3d=False)
+
