d6648cd2d5096d8843c34475e4c9365d9c88c8fe
Marc Rosanes <marcrosanes@gmail.com>
2022-09-03 17:26:10 +0200

    Update function call in test

--- test_2D_specific_intensity.py
+++ test_2D_specific_intensity.py
@@ -2,7 +2,8 @@
 from object.object_main_func import specific_intensities_2D
 
 specific_intensities_2D(
-        L=30, n=60, beta=65, rotation_angle=0, inclination=25,
-        Robj_Rsun_scale=4, Bp=7700, Pr=1, D_pc=373, f=5e9, Ra=14,
-        l_middlemag=4, δ=2, r_ne=0.002, v_inf=600, plot3d=False)
+        L=30, n=80, beta=0, rotation_angle=0, inclination=90,
+        Robj_Rsun_scale=4, Bp=7700, Pr=1, D_pc=373, f=5e9, Ra=16,
+        l_middlemag=4, δ=2, r_ne=0.002, v_inf=600, inner_contrib=True,
+        n_p0=1e8, T_p0=1e6, plot3d=False)
 
