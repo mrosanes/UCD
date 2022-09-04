9988b9d60e3537e898d715475475b047d2d5aa42
Marc Rosanes <marcrosanes@gmail.com>
2022-09-04 22:25:29 +0200

    Update test with new option using symmetry
    Using symmetry for 1D plots requires an extra input parameter

--- test_1D_flux_density.py
+++ test_1D_flux_density.py
@@ -2,8 +2,8 @@
 from object.object_main_func import flux_densities_1D
 
 flux_densities_1D(
-    L=40, n=40, beta=65, inclination=25, Robj_Rsun_scale=4, Bp=7700, Pr=1,
+    L=40, n=41, beta=65, inclination=25, Robj_Rsun_scale=4, Bp=7700, Pr=1,
     D_pc=373, f=5e9, Ra=15, l_middlemag=7, δ=2, r_ne=0.002, v_inf=600,
     rotation_angle_step=30, inner_contrib=True, n_p0=3e9, T_p0=1e5,
-    plot3d=False, mk_new_qapp=True)
+    use_symmetry=True, plot3d=False, mk_new_qapp=True)
 
