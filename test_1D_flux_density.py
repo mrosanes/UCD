98dfa2a20b91abf92344b6f748c428a57fd70ea7
Marc Rosanes <marcrosanes@gmail.com>
2022-09-04 20:18:50 +0200

    Update test parameters

--- test_1D_flux_density.py
+++ test_1D_flux_density.py
@@ -2,7 +2,8 @@
 from object.object_main_func import flux_densities_1D
 
 flux_densities_1D(
-    L=30, n=70, beta=65, inclination=25, Robj_Rsun_scale=4, Bp=7700, Pr=1,
-    D_pc=373, f=5e9, Ra=14, l_middlemag=6, δ=2, r_ne=0.002, v_inf=600,
-    rotation_angle_step=10, plot3d=False, mk_new_qapp=True)
+    L=40, n=40, beta=65, inclination=25, Robj_Rsun_scale=4, Bp=7700, Pr=1,
+    D_pc=373, f=5e9, Ra=15, l_middlemag=7, δ=2, r_ne=0.002, v_inf=600,
+    rotation_angle_step=30, inner_contrib=True, n_p0=3e9, T_p0=1e5,
+    plot3d=False, mk_new_qapp=True)
 
--- test_2D_specific_intensity.py
+++ test_2D_specific_intensity.py
@@ -1,11 +1,9 @@
 
 from object.object_main_func import specific_intensities_2D
 
-# MCP Star HD37479: rotation_angle = -self.phi_r + 72*np.pi/180.0
-# MCP Star HD37017: -self.phi_r + np.pi/180.0
 specific_intensities_2D(
-        L=20, n=40, beta=65, rotation_angle=324, inclination=25,
+        L=40, n=50, beta=65, rotation_angle=0, inclination=25,
         Robj_Rsun_scale=4, Bp=7700, Pr=1, D_pc=373, f=5e9, Ra=14,
-        l_middlemag=6, δ=2, r_ne=0.002, v_inf=600, inner_contrib=False,
-        n_p0=1e8, T_p0=1e6, plot3d=False)
+        l_middlemag=6, δ=2, r_ne=0.002, v_inf=600, inner_contrib=True,
+        n_p0=3e9, T_p0=1e5, plot3d=False)
 
