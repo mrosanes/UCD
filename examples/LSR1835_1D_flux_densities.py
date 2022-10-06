56fb1f3dfea9453e334a4ab8e9772e080809c22f
Marc Rosanes <marcrosanes@gmail.com>
2022-10-07 01:18:16 +0200

    Display two pyqtgraph plots
    Display two pyqtgraph plots:
    
    1- With the observed light curve, and the ligth curve computed
    with i=140º and beta=50º
    
    2- With the observed light curve, and the ligth curve computed
    with i=150º and beta=30º: improving the fit between observed and
    computed curves

--- examples/LSR1835_1D_flux_densities.py
+++ examples/LSR1835_1D_flux_densities.py
@@ -23,44 +23,59 @@ import pyqtgraph as pg
 
 ###############################################################################
 # Data from a real astronomic observation of LSRJ1835+3259 (RCP):
+rotation_phases_observed = [
+    0.0469, 0.1435, 0.2431, 0.3808, 0.5565, 0.7323, 0.9080, 1.0838, 1.2595,
+    1.4353, 1.6110, 1.8571]
 
-rotation_phases_0 = [
- 0, 0.0937, 0.1933, 0.2929, 0.4686, 0.6444, 0.8201, 0.9959, 1.1716,
- 1.3474, 1.5231, 1.6989
-]
 # Flux densities in mJy:
-flux_densities_0 = [
- 0.4351, 0.4331, 0.3644, 0.3375, 0.2142, 0.2679, 0.2248, 0.3011, 0.3305,
- 0.2917, 0.2315, 0.1886]
+flux_densities_observed = [
+    0.4351, 0.4331, 0.3644, 0.3375, 0.2142, 0.2679, 0.2248, 0.3011, 0.3305,
+    0.2917, 0.2315, 0.1886]
 
 ###############################################################################
 # Data obtained using the software:
 # L = 15; Bp = 5000; neA = 5416; Ra = 31
 
-# Rotation phase [0 - 2PI]
-rotation_angles = [
- 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160,
- 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310,
- 320, 330, 340, 350, 360]
-
+rotation_angles = []
+for angle in range(-50, 610, 10):
+    rotation_angles.append(angle)
 # Rotation phase: [0-1]  ([0-1] -> [0º-360º])
-rotation_phases = 0.13 + np.array(rotation_angles) / 360.0
+rotation_phases = 0.23 + np.array(rotation_angles) / 360.0
 
 # Flux densities as a function of the rotation phase:
-flux_densities = 20 * np.array([
- 0.016, 0.019, 0.019, 0.019, 0.024, 0.02, 0.019, 0.021, 0.018, 0.021, 0.017,
- 0.017, 0.016, 0.015, 0.016, 0.011, 0.011, 0.01, 0.011, 0.01, 0.011, 0.011,
- 0.016, 0.015, 0.016, 0.017, 0.017, 0.021, 0.018, 0.021, 0.019, 0.02, 0.024,
- 0.019, 0.019, 0.019, 0.016])
+flux_densities_initial_i140_beta50 = 20 * np.array([
+    0.024, 0.019, 0.019, 0.019, 0.016,
+    0.016, 0.019, 0.019, 0.019, 0.024, 0.02, 0.019, 0.021, 0.018, 0.021, 0.017,
+    0.017, 0.016, 0.015, 0.016, 0.011, 0.011, 0.01, 0.011, 0.01, 0.011, 0.011,
+    0.016, 0.015, 0.016, 0.017, 0.017, 0.021, 0.018, 0.021, 0.019, 0.02, 0.024,
+    0.019, 0.019, 0.019, 0.016,
+    0.016, 0.019, 0.019, 0.019, 0.024, 0.02, 0.019, 0.021, 0.018, 0.021,
+    0.017, 0.017, 0.016, 0.015, 0.016, 0.011, 0.011, 0.01, 0.011, 0.01,
+    0.011, 0.011, 0.016, 0.015])
+
+flux_densities_fitted_i150_beta30 = 20 * np.array(
+    [0.021, 0.019, 0.019, 0.021, 0.021,
+     0.021, 0.021, 0.019, 0.019, 0.021, 0.018, 0.019, 0.018, 0.019, 0.017,
+     0.016, 0.015, 0.017, 0.011, 0.013, 0.012, 0.011, 0.012, 0.012, 0.012,
+     0.011, 0.012, 0.013, 0.011, 0.017, 0.015, 0.016, 0.017, 0.019, 0.018,
+     0.019, 0.018, 0.021, 0.019, 0.019, 0.021, 0.021,
+     0.021, 0.021, 0.019, 0.019, 0.021, 0.018, 0.019, 0.018, 0.019, 0.017,
+     0.016, 0.015, 0.017, 0.011, 0.013, 0.012, 0.011, 0.012, 0.012, 0.012,
+     0.011, 0.012, 0.013, 0.011])
 
 ###############################################################################
 
 # Average Flux Densities (in mJy)
-FD_avg = np.average(flux_densities)
-print("\nAverage Flux Density:")
-print(" {:.4g}".format(FD_avg))
+FD_avg_initial = np.average(flux_densities_initial_i140_beta50)
+print("\nAverage Flux Density Initial (inclination=140º; beta=50º):")
+print(" {:.4g}".format(FD_avg_initial))
 print()
 
+# Average Flux Densities Fitted (in mJy)
+FD_avg_fitted = np.average(flux_densities_fitted_i150_beta30)
+print("\nAverage Flux Density Fitted (inclination=150º; beta=30º):")
+print(" {:.4g}".format(FD_avg_fitted))
+print()
 ###############################################################################
 # Plotting both light curves (real observation and computed), in
 # the same graph:
@@ -69,13 +84,22 @@ app = pg.mkQApp()
 plt = pg.plot()
 
 orange = (217, 83, 25)
-plt.plot(rotation_phases_0, flux_densities_0,
+plt.plot(rotation_phases_observed, flux_densities_observed,
          pen=pg.mkPen(orange, width=2),
          symbol='o', symbolPen='w', symbolBrush=orange)
-plt.plot(rotation_phases, flux_densities,
+plt.plot(rotation_phases, flux_densities_initial_i140_beta50,
          pen=pg.mkPen('b', width=2),
-         symbol='o', symbolPen='w')
-
+         symbol='o')
 plt.setWindowTitle("Flux Densities")
+
+plt2 = pg.plot()
+plt2.plot(rotation_phases_observed, flux_densities_observed,
+          pen=pg.mkPen(orange, width=2),
+          symbol='o', symbolPen='w', symbolBrush=orange)
+plt2.plot(rotation_phases, flux_densities_fitted_i150_beta30,
+          pen=pg.mkPen('b', width=2),
+          symbol='o')
+plt2.setWindowTitle("Flux Densities Fitted")
+
 app.exec_()
 
